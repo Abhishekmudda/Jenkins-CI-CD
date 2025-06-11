51f97e9d4368898914993f5865c2967ed7402fc8
Your Name <abhimudda2003@gmail.com>
2025-06-11 18:49:50 +0530

    change port number to 5003

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def home():
     return render_template('index.html', result=result)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5003, debug=True)
