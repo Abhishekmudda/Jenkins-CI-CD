3d7d5ed6b35fe493c22acac9ce9783b50bf04d42
Your Name <abhimudda2003@gmail.com>
2025-06-11 18:45:01 +0530

    initial commit to app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+def even_or_odd(num):
+    if num < 1:
+        return "Invalid Input"
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    result = None
+    if request.method == 'POST':
+        try:
+            num = int(request.form['number'])
+            result = even_or_odd(num)
+        except:
+            result = "Invalid Input"
+    return render_template('index.html', result=result)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
